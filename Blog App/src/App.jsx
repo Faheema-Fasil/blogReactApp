857d1dfb5a86943a733e6fae4505e6e037417044
Faheema_Fasil <faheemafasil12@gmail.com>
2024-10-28 09:26:51 +0530

    updates

--- Blog App/src/App.jsx
+++ Blog App/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import  Posts  from "./componets/Posts";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>qwer</h1>
+   <Home/>
+   <Posts/>
+   <Footer/>
     </>
   )
 }
